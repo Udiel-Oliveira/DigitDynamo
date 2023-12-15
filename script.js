7c84d76ff2ab83b610e739c6044bd24e27f48751
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2023-12-15 10:09:32 -0300

    adicionando a feature relogio

---
+++ script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementById('hours');
+const minutos = document.getElementById('minutes');
+const segundos = document.getElementById('seconds');
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (sec < 10) sec = '0' + sec;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+})
