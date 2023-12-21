5010ad4db6b459094b8565ac200b1d43bfd0013e
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2023-12-21 08:35:10 -0300

    adicionando o header ao site

--- script.js
+++ script.js
@@ -231,3 +231,19 @@ function copiar(){
 
 
 
+
+
+
+
+
+
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) => 
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) => 
+    item.addEventListener('click', activeLink)
+);
